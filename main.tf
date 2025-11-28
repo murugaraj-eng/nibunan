65bbe7e2389f2d6b5304aea4f24979d1500fe2db
murugaraj-eng <murugaraj@msn.com>
2025-11-28 13:13:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,75 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-provider "aws" {
-  region = var.region
+# Configure the OCI Provider
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  private_key_path = var.private_key_path
+  fingerprint      = var.fingerprint
+  region           = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+# Define variables
+variable "compartment_ocid" {
+  description = "ocid1.compartment.oc1..aaaaaaaa5t75sxcdxlzfydz4ljjz6bwzzrmprpt6re6oshibsyieozxmsosa"
+  type        = string
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+variable "subnet_ocid" {
+  description = "ocid1.subnet.oc1.ap-hyderabad-1.aaaaaaaapoeq4s2vcdmzrwvan7yv5zciqddojhablzt7et6iizin772dqq5a"
+  type        = string
+}
 
-  tags = {
-    Name = var.instance_name
+variable "image_ocid" {
+  description = "ocid1.image.oc1.ap-hyderabad-1.aaaaaaaau3h2vfcaa6jwposxo35e7iqxt5wmmbgvdondfm6c2dczep5t2s2q"
+  type        = string
+}
+
+variable "shape" {
+  description = "VM.Standard.E2.1.Micro"
+  type        = string
+}
+
+variable "display_name" {
+  description = "TF-Instance"
+  type        = string
+  default     = "my-terraform-instance"
+}
+
+variable "ssh_public_key" {
+  description = "The SSH public key for accessing the instance."
+  type        = string
+}
+
+# Create the OCI Compute Instance
+resource "oci_core_instance" "my_instance" {
+  compartment_id        = var.compartment_ocid
+  availability_domain   = data.oci_identity_availability_domains.ads.availability_domains[0].name # Or specify a specific AD
+  shape                 = var.shape
+  display_name          = var.display_name
+
+  source_details {
+    source_type = "image"
+    image_id    = var.image_ocid
   }
+
+  create_vnic_details {
+    subnet_id        = var.subnet_ocid
+    assign_public_ip = true # Set to false if you don't need a public IP
+  }
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+  }
+}
+
+# Data source to retrieve Availability Domains
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.tenancy_ocid
+}
+
+# Output the public IP address of the instance
+output "instance_public_ip" {
+  value = oci_core_instance.my_instance.public_ip
 }
