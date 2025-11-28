1f9624a04aafd0d66d854ce209b49746ad3f60b1
murugaraj-eng <murugaraj@msn.com>
2025-11-28 19:28:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "oci_core_route_table" "example_rt" {
 
 # Update Subnet with Route Table
 resource "oci_core_subnet" "example_subnet_with_rt" {
-  cidr_block        = "20.0.4.0/24"
+  cidr_block        = "20.0.5.0/24"
   display_name      = "example-subnet"
   vcn_id            = oci_core_vcn.example_vcn.id
   compartment_id    = var.compartment_ocid
