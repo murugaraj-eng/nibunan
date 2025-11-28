61c191fa65c33c4729e4f0efdfa8bc82cc18ff8e
murugaraj-eng <murugaraj@msn.com>
2025-11-28 16:44:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,25 @@
 variable "tenancy_ocid" {
+description = {ocid1.tenancy.oc1..aaaaaaaaztjcu7ullfmozcfqr4ir5xiqwk7hpiagzo4vbknezkbtv6ms63wq}
 type = string
-default = {ocid1.tenancy.oc1..aaaaaaaaztjcu7ullfmozcfqr4ir5xiqwk7hpiagzo4vbknezkbtv6ms63wq}
 }
 variable "user_ocid" {
+description = {ocid1.user.oc1..aaaaaaaanjnckzglk2fjel3uq7em4rpiz4ttufox7ss33x32isjkm6kz6ltq} 
 type = string
-default = {ocid1.user.oc1..aaaaaaaanjnckzglk2fjel3uq7em4rpiz4ttufox7ss33x32isjkm6kz6ltq} 
+
 }
 variable "fingerprint" {
+description = {8b:47:fd:3c:11:94:fe:84:09:75:09:34:47:1c:dd:aa}
 type = string
-default = {8b:47:fd:3c:11:94:fe:84:09:75:09:34:47:1c:dd:aa}
+
 }
 #variable "private_key_path" {}
+
 variable "region" {
+description = {ap-hyderabad-1}
 type = string
-default = {ap-hyderabad-1}
 }
+
 variable "compartment_ocid" {
-type = string
-default = {ocid1.compartment.oc1..aaaaaaaa5t75sxcdxlzfydz4ljjz6bwzzrmprpt6re6oshibsyieozxmsosa}
+  description = {ocid1.compartment.oc1..aaaaaaaa5t75sxcdxlzfydz4ljjz6bwzzrmprpt6re6oshibsyieozxmsosa}
+  type  = string
 }
