92c44cb690540780fe5e2bba7abc8e33b8abe759
murugaraj-eng <murugaraj@msn.com>
2025-11-28 13:17:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,35 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-variable "region" {
-  description = "AWS region"
-  default     = "us-west-1"
+# Define variables
+variable "compartment_ocid" {
+  description = "ocid1.compartment.oc1..aaaaaaaa5t75sxcdxlzfydz4ljjz6bwzzrmprpt6re6oshibsyieozxmsosa"
+  type        = string
 }
 
-variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+variable "subnet_ocid" {
+  description = "ocid1.subnet.oc1.ap-hyderabad-1.aaaaaaaapoeq4s2vcdmzrwvan7yv5zciqddojhablzt7et6iizin772dqq5a"
+  type        = string
 }
 
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+variable "image_ocid" {
+  description = "ocid1.image.oc1.ap-hyderabad-1.aaaaaaaau3h2vfcaa6jwposxo35e7iqxt5wmmbgvdondfm6c2dczep5t2s2q"
+  type        = string
+}
+
+variable "shape" {
+  description = "VM.Standard.E2.1.Micro"
+  type        = string
+}
+
+variable "display_name" {
+  description = "TF-Instance"
+  type        = string
+  default     = "my-terraform-instance"
+}
+
+variable "ssh_public_key" {
+  description = "The SSH public key for accessing the instance."
+  type        = string
 }
 
